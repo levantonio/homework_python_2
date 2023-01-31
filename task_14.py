77633c8d0a51b3925546f6238ec5516224cb1e6f
Anton <levantonio@mail.ru>
2023-01-31 14:17:47 +0300

    task14

--- task_14.py
+++ task_14.py
@@ -2,3 +2,14 @@
 # вида 2k
 # ), не превосходящие числа N.
 # 10 -> 1 2 4 8
+import random as r
+
+n = r.randint(1, 100)
+result = 1
+arr = []
+for i in range(1, n):
+    if result < n:
+        arr.append(result)
+        result = 2 ** i
+
+print(f"{n} -> {arr}")
